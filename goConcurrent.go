8d4b5f29797f8f9cb8f208718c81323dc57971a2
Shaun <shaun@kloudimi.com>
2021-06-18 11:45:04 +0200

    Concurrent primitive wrappers

---
+++ goConcurrent.go
@@ -0,0 +1,112 @@
+package GoConcurrent
+
+import "sync"
+
+// Or
+func Or(channels ...<-chan interface{}) <-chan interface{} {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+	go func() {
+		defer close(orDone)
+
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-channels[2]:
+			case <-or(append(channels[3:], orDone)...):
+			}
+		}
+	}()
+	return orDone
+}
+
+// Repeat
+func Repeat(done <-chan interface{}, values ...interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			for _, v := range values {
+				select {
+				case <-done:
+					return
+				case valStream <- v:
+				}
+			}
+		}
+	}()
+	return valStream
+}
+
+// RepeatFn
+func RepeatFn(done <-chan interface{}, fn func() interface{}) <-chan interface{} {
+	valStream := make(chan interface{})
+	go func() {
+		defer close(valStream)
+		for {
+			select {
+			case <-done:
+				return
+			case valStream <- fn():
+			}
+		}
+	}()
+	return valStream
+}
+
+// Take
+func Take(done <-chan interface{}, valStream <-chan interface{}, num int) <-chan interface{} {
+	takeStream := make(chan interface{})
+	go func() {
+		defer close(takeStream)
+		for i := 0; i < num; i++ {
+			select {
+			case <-done:
+				return
+			case takeStream <- <-valStream:
+			}
+		}
+	}()
+	return takeStream
+}
+
+// FanIn
+func FanIn(done <-chan interface{}, channels ...<-chan interface{}) <-chan interface{} {
+	var wg sync.WaitGroup
+	multiPlexedStream := make(chan interface{})
+
+	multiPlex := func(c <-chan interface{}) {
+		defer wg.Done()
+		for i := range c {
+			select {
+			case <-done:
+				return
+			case multiPlexedStream <- i:
+			}
+		}
+	}
+
+	wg.Add(len(channels))
+	for _, c := range channels {
+		go multiPlex(c)
+	}
+
+	go func() {
+		wg.Wait()
+		close(multiPlexedStream)
+	}()
+	return multiPlexedStream
+}
